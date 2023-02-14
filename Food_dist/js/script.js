960bdd42215b58847d6849fbb57c04c91ea90cc5
Mkrtich <amv.mkrtich@gmail.com>
2023-02-14 19:49:46 +0400

    calculator

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+    function numberFormatting(num){
+        return num < 10 ? '0' + num : num;
+    }
+    function str2num(str){
+        return +(str.replace(/\D+/, ''));
+    }
+
+    
     // Tabs
     function tabRealization() {
         const tabContent = document.querySelectorAll('.tabcontent'),
@@ -71,10 +79,10 @@ document.addEventListener('DOMContentLoaded', () => {
             //     t.seconds = '0';
             // }
             
-            days.innerHTML = t.days < 10 ? '0' + t.days : t.days;
-            hours.innerHTML = t.hours < 10 ? '0' + t.hours : t.hours;
-            minutes.innerHTML = t.minutes < 10 ? '0' + t.minutes : t.minutes;
-            seconds.innerHTML = t.seconds < 10 ? '0' + t.seconds : t.seconds;
+            days.innerHTML = numberFormatting(t.days);
+            hours.innerHTML = numberFormatting(t.hours);
+            minutes.innerHTML = numberFormatting(t.minutes);
+            seconds.innerHTML = numberFormatting(t.seconds);
             
             if(t.total <= 0) clearInterval(timeInterval);
         }
@@ -115,7 +123,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 22000);
+    // const modalTimerId = setTimeout(openModal, 22000);
 
     function showModalOnScroll(){
         if(window.pageYOffset + document.documentElement.clientHeight >= document.body.scrollHeight - 5){
@@ -215,36 +223,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new MenuCart(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container'
-    ).render();
+    const getResources = async (url) => {
+          const res = await fetch(url);
 
-    new MenuCart(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        20,
-        '.menu .container',
-        'menu-item-has-children',
-        'menu__item'
-    ).render();
+          if(!res.ok){
+              throw new Error(`Cound not fetch ${url}, status: ${res.status}`);
+          }
 
-    new MenuCart(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
-        16,
-        '.menu .container'
-    ).render();
-    
-    
+          return await res.json();
+    };
+
+    /*getResources('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, alt, title, descr, price}) => {
+                // let {img, alt, title, desc, price} = obj;
+                new MenuCart( img, alt, title, descr, price, '.menu .container' ).render();
+            });
+        });*/
+
+    axios.get('http://localhost:3000/menu')
+        .then(resp => {
+            resp.data.forEach(({img, alt, title, descr, price}) => {
+                new MenuCart(img, alt, title, descr, price, '.menu .container').render();
+            });
+        });
+
     // Forms
     const forms = document.querySelectorAll('form'),
         message = {
@@ -316,4 +319,218 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal();
         },3000);
     }
+
+    // slider
+    const slider = document.querySelector('.offer__slider'),
+        prev = slider.querySelector('.offer__slider-prev'),
+        next = slider.querySelector('.offer__slider-next'),
+        slides = slider.querySelectorAll('.offer__slide'),
+        slidesWrapper = slider.querySelector('.offer__slider-wrapper'),
+        slidesField = slider.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
+    let slideIndex = 1,
+        offset = 0;
+
+    slidesWrapper.style.overflow = 'hidden';
+    slidesField.style.cssText = `
+        display: flex;
+        width: ${slides.length*100}%;
+        transition: all .5s ease;
+    `;
+    slider.style.position = 'relative';
+    slides.forEach(item => item.style.width = width);
+    slider.querySelector('#current').textContent = numberFormatting(slideIndex);
+    slider.querySelector('#total').textContent = numberFormatting(slides.length);
+
+    /*showSlide(slideIndex);
+
+
+    function showSlide(n){
+        if(n > slides.length){
+            slideIndex = 1;
+        }
+        if(n < 1){
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+
+        slider.querySelector('#current').textContent = numberFormatting(slideIndex);
+    }
+
+    function changeSlides(n){
+        showSlide(slideIndex += n);
+    }
+*/
+
+    // dots
+    const dots = document.createElement('ol');
+    dots.classList.add('carousel-dots');
+    let dotsInner = '';
+    for(let i = 0; i < slides.length; i++){
+        let isActive = '';
+        if(i+1 === slideIndex){
+            isActive = 'is-active';
+        }
+        dotsInner += `
+            <li class="dot ${isActive}" data-dot-index="${i+1}"></li>
+        `;
+    }
+    dots.innerHTML = dotsInner;
+    slider.append(dots);
+
+    function setDotActive(dotIndex){
+        dots.querySelectorAll('.dot').forEach((dot, i) => {
+            dot.classList.remove('is-active');
+            if(i+1 == dotIndex) {
+                dot.classList.add('is-active');
+            }
+        });
+    }
+
+    dots.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(e.target.closest('.dot')){
+            slideIndex = e.target.dataset.dotIndex;
+            setDotActive(slideIndex);
+            offset = str2num(width) * (slideIndex - 1);
+            slidesField.style.transform = `translateX(-${offset}px)`;
+            slider.querySelector('#current').textContent = numberFormatting(slideIndex);
+        }
+    });
+
+    slider.querySelector('.offer__slider-counter').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if( e.target.closest('.offer__slider-prev') ){
+            if(offset == 0){
+                offset = str2num(width) * (slides.length - 1);
+            }else{
+                offset -= str2num(width);
+            }
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if(slideIndex == 1){
+                slideIndex = slides.length;
+            }else{
+                slideIndex--;
+            }
+            slider.querySelector('#current').textContent = numberFormatting(slideIndex);
+            setDotActive(slideIndex);
+        }
+        if( e.target.closest('.offer__slider-next') ){
+            if(offset === str2num(width) * (slides.length - 1)){
+                offset = 0;
+            }else{
+                offset += str2num(width);
+            }
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if(slideIndex == slides.length){
+                slideIndex = 1;
+            }else{
+                slideIndex++;
+            }
+            slider.querySelector('#current').textContent = numberFormatting(slideIndex);
+            setDotActive(slideIndex);
+        }
+    });
+
+    // calculator
+    const result = document.querySelector('.calculating__result span');
+    let sex = localStorage.getItem('sex') ? localStorage.getItem('sex') : 'female',
+        height,
+        weight,
+        age,
+        ratio = localStorage.getItem('ratio') ? localStorage.getItem('ratio') : 1.375;
+    localStorage.setItem('sex', sex);
+    localStorage.setItem('ratio', ratio);
+
+    function calcTotal(){
+        if(!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if(sex === 'female'){
+            result.textContent = ((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio).toFixed(2);
+        }else{
+            result.textContent = ((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio).toFixed(2);
+        }
+    }
+
+    function initLocalSettings(selector, activeClass){
+        const elems = document.querySelectorAll(selector);
+
+        elems.forEach(el => {
+            el.classList.remove(activeClass);
+            if(el.getAttribute('id') === localStorage.getItem('sex')){
+                el.classList.add(activeClass);
+            }
+            if(el.dataset.ratio === localStorage.getItem('ratio')){
+                el.classList.add(activeClass);
+            }
+        });
+    }
+    
+    calcTotal();
+
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+    getStaticInfo('#gender');
+    getStaticInfo('.calculating__choose_big');
+
+    getDynamicInfo('#height');
+    getDynamicInfo('#weight');
+    getDynamicInfo('#age');
+
+    function getStaticInfo(parent, activeClass='calculating__choose-item_active'){
+        const elems = document.querySelectorAll(`${parent} div`);
+        document.querySelector(parent).addEventListener('click', (e) => {
+            if(!e.target.closest('.calculating__choose-item')) return false;
+
+            if(e.target.hasAttribute('data-ratio')){
+                ratio = +e.target.getAttribute('data-ratio');
+                localStorage.setItem('ratio', ratio);
+            }else{
+                sex = e.target.getAttribute('id');
+                localStorage.setItem('sex', sex);
+            }
+
+            elems.forEach(el => {
+                el.classList.remove(activeClass);
+            });
+            e.target.classList.add(activeClass);
+
+            calcTotal();
+        });
+
+    }
+
+    function getDynamicInfo(selector){
+        const input = document.querySelector(selector);
+        input.addEventListener('input', (e) => {
+            if(input.value.search(/\D/) !== -1){
+                input.style.backgroundColor = '#f00';
+            }else{
+                input.style.backgroundColor = '';
+            }
+            switch (input.getAttribute('id')){
+                case 'height': 
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+
+            calcTotal();
+        });
+
+    }
 });
+
